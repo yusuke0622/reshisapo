8fb6d3d26e3036e88eb235cb1adee6a22e9284b3
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-07-14 15:13:01 +0000

    Add user_edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,12 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
     
+    def after_sign_in_path_for(resource)
+        my_page_path
+    end
+    
+    def after_sign_out_path_for(resoutce)
+        new_user_session_path
+    end
     
 end

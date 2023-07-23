bdabecbcc096885bbac2b89b1209b8e23ec8e19a
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-07-23 06:45:39 +0000

    Add recipe_edit

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -7,6 +7,7 @@ class Public::RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @recipe_tags = @recipe.tags
+    @category = Category.find_by(params[:id])
   end
 
   def new
@@ -16,7 +17,7 @@ class Public::RecipesController < ApplicationController
   end
   
   def create
-    @recipe = current_user.recipes.build(recipe_params)
+    @recipe = current_user.recipes.new(recipe_params)
     tag_list = params[:recipe][:tag_ids].split(nil)
     if @recipe.save
       @recipe.save_tag(tag_list)
@@ -25,11 +26,24 @@ class Public::RecipesController < ApplicationController
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
+    @tag_list = @recipe.tags.pluck(:tag_name).join(nil)
+  end
+  
+  def update
+    recipe = Recipe.find(params[:id])
+    if recipe.update(recipe_params)
+      flash[:success] = "投稿内容を変更しました"
+      redirect_to recipe_path(recipe.id)
+    else 
+      render :edit
+    end
   end
   
   private
     def recipe_params
-      params.require(:recipe).permit(:user_id, :recipe_name, :introduction, :serving, :category_id, 
-      ingredients_attributes:[:id, :recipe_id, :ingredient_name, :quantity, :_destroy])
+      params.require(:recipe).permit(:user_id, :recipe_name, :introduction, :serving, :category_id, :recipe_image, :step_image,
+      ingredients_attributes:[:id, :recipe_id, :ingredient_name, :quantity, :_destroy],
+      steps_attributes:[:id, :recipe_id, :explanation, :step_image, :number, :_destroy])
     end
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,4 @@
 class Step < ApplicationRecord
     belongs_to :recipe
+    has_one_attached :step_image
 end

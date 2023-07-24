bc22c944d7571257183f66bee9d0c856dd738d76
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-07-24 15:54:49 +0000

    Add search_form

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,17 @@
 class Category < ApplicationRecord
     has_many :recipes, dependent: :destroy    
+    
+ def self.looks(search, word)
+   if search == "perfect_match"
+     @category = Category.where("name LIKE?", "#{word}")
+   elsif search == "forward_match"
+     @category = Category.where("name LIKE?", "#{word}%")
+   elsif  search == "backward_match"
+     @category = Category.where("name LIKE?", "%#{word}")
+   elsif search == "partial_match"
+     @category = Category.where("name LILE?", "%#{word}%")
+   else
+     @category = Category.all
+   end
+ end
 end

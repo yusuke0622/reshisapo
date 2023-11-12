01d47e0e9a146d4b4305c5c2f9d61e50618aefe2
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-11-12 06:33:57 +0000

    Update routes_layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,62 @@
 Rails.application.routes.draw do
   
-
-  
-  #管理者用
+  ##　サインアップ・サインイン
+  #管理者
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
-  #ユーザー用
+  #ユーザー
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
   
-  
-  #トップページ
+  ## トップページ
+  # 管理者
   get 'admin' => 'admin/homes#top'
+  # ユーザー
   root to: 'public/homes#top'
   
-  get 'category_detail' => 'public/homes#category_detail'
-  #カテゴリー
+  ## 管理者ページ
   namespace :admin do
+    #ユーザー
     resources :users, only: [:show, :index, :destroy]
     patch 'withdraw/:id' => 'users#withdraw', as: "withdraw"
+    #レシピ
     resources :recipes, only: [:show, :index, :destroy] do
       resources :comments, only: [:destroy]
     end
+    #カテゴリー・カテゴリー検索
     resources :categories, except: [:new, :show] do 
        get 'recipes' => 'recipes#search_category'
     end
+    #タグ・タグ検索
     resources :tags do 
        get 'recipes' => 'recipes#search_tag'
     end
   end
   
+  ##　ユーザー用ページ
+  # カテゴリー詳しく
+  get 'category_detail' => 'public/homes#category_detail'
+  
   scope module: :public do
-    #ユーザー
+    # ユーザー
     get   'users/information/edit'  => 'users#edit'
     patch 'users/information'       => 'users#update'
     get   'users/quit'              => 'users#quit'
     patch 'users/withdraw'          => 'users#withdraw'
     resources :users, only:[:index, :show] do 
+      # お気に入りレシピ一覧
       member do 
-        get :favorites
+        get :favorites　　
       end
     end
     # レシピ
     resources :recipes do
+      # お気に入り登録・解除
       resource :favorites, only: [:create, :destroy]
+      # コメント
       resources :comments, only: [:create, :edit, :update, :destroy]
     end
     #タグ絞り込み
@@ -66,13 +75,6 @@ Rails.application.routes.draw do
   #検索
   get "admin/search" => "admin/searches#search"
   get "search" => "public/searches#search"
-  
-  
-  
-  
- 
-  
-  #いいね
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
